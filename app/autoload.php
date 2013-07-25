3e9b59d728cecb77454d0d4d2fb46861caeb5eef
tadek <tadeusz.magiera@gmail.com>
2013-07-25 21:18:21 +0100

    MongoDB support

--- app/autoload.php
+++ app/autoload.php
@@ -1,7 +1,8 @@
 <?php
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 
 /**
  * @var $loader ClassLoader
